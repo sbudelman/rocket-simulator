24e8c7b9fb7279766400d3689f79fd927e388330
Samuel Udelman <samueludelman@gmail.com>
2022-01-09 23:13:23 +0100

    theta graph and multicolumn subplots

--- src/graphs.py
+++ src/graphs.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math as m
 
+MAX_ROWS = 3
 
 class GraphHandler:
     def __init__(self, n_plots, **kwargs):
-        self.rows = n_plots
-        self.cols = 1
+        self._max_rows = MAX_ROWS
+        self.rows = min(n_plots, self._max_rows) 
+        self.cols = m.ceil(n_plots/self._max_rows)
         self.data_size = kwargs.get('size', [0, 0])
         self.data_array = np.zeros((self.data_size[1], self.data_size[0]))
-        
         self.fig, self.axs = plt.subplots(self.rows, self.cols)
         self.fig.subplots_adjust(hspace=1.5)
 
@@ -20,7 +22,9 @@ class GraphHandler:
         self._current_idx += 1
 
     def define_plot(self, x_col, y_col, **kwargs):
-        ax = self.axs[self._plot_idx]
+        plot_row = m.floor(self._plot_idx / self.cols)
+        plot_col = self._plot_idx % self.cols
+        ax = self.axs[plot_row][plot_col]
         # We use self._current_idx - 1 to remove initialized rows that weren't used
         ax.plot(self.data_array[:self._current_idx - 1,x_col], self.data_array[:self._current_idx - 1,y_col])
         ax.set_xlabel(kwargs.get('xlabel', ''))
